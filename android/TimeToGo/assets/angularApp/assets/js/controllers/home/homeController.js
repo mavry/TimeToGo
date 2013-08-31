d7b9e4d93e0d57ec0c20922cf20cbcad4b3eb6fe
yonatanm <yonatanm@gmail.com>
2013-08-31 15:05:20 +0300

    more files

--- android/TimeToGo/assets/angularApp/assets/js/controllers/home/homeController.js
+++ android/TimeToGo/assets/angularApp/assets/js/controllers/home/homeController.js
@@ -2,13 +2,40 @@
 
 /* Controllers */
 
-function HomeCtrl($scope, $location) {
+function HomeCtrl($scope, $rootScope, $location, Backend) {
+
+
+	$scope.showStartLocation = false;
+
+	$scope.onStartLocationClick = function(){
+	  $scope.showStartLocation = true;
+	}
+
+	$scope.obtainCurrentLocation = function() {
+	  $rootScope.waitingForLocation = true;
+	}
+
+	$scope.startLocation={
+		input:""
+	};
+
+	$scope.onCurrentLocation = function(x, y) {
+	  console.log("in onCurrentLocation()");
+      $rootScope.waitingForLocation = false;
+	};
+	
+//	setTimeout(function () { $scope.fromAndroiad.onCurrentLocation("123","456"); }, 6000);
+
+
+    $rootScope.waitingForLocation = false;
 
 	$scope.submit = function ()
 	{
-		console.log("in submit");
-		 $location.path("route");
+	  //console.log("in submit Backend doIt = "+Backend.doIt());
+
+	 // $location.path("route");
 	};
+
 
 }
 
--- android/TimeToGo/assets/angularApp/assets/js/timeToGo.js
+++ android/TimeToGo/assets/angularApp/assets/js/timeToGo.js
@@ -2,19 +2,39 @@
 
 /* App Module */
 
-// angular.module('timeToGo', []).
-//   config(['$routeProvider', function($routeProvider) {
-angular.module('timeToGo', [], function($routeProvider, $locationProvider) {	
-  $routeProvider.
-      when('/home', 
-      	{templateUrl: 'assets/templates/home/home.html',   controller: 'HomeCtrl'}
-      ).when('/config', 
-      	{templateUrl: 'assets/templates/config/config.html',   controller: 'ConfigCtrl'}
-      ).when('/route', 
-      	{templateUrl: 'assets/templates/route/route.html',   controller: 'RouteCtrl'}
-      ).otherwise(
-      	{redirectTo: '/home'}
-      );
+var ROOT;
+
+
+var mockedAndroidInterface = {};
+
+angular.module('timeToGo', ["backendServices", "simulatorServices"], 
+  function($routeProvider, $locationProvider ) {	
+    $routeProvider.
+      when('/home', {templateUrl: 'assets/templates/home/home.html', controller: 'HomeCtrl'}).
+      when('/config', {templateUrl: 'assets/templates/config/config.html', controller: 'ConfigCtrl'}).
+      when('/route',  {templateUrl: 'assets/templates/route/route.html', controller: 'RouteCtrl'}).
+      otherwise({redirectTo: '/home'});
+  }
+).run(function ($rootScope) {
+
+  ROOT = $rootScope;
+
+  ROOT.fromAndroiad = {
+    onCreate : function() {
+        console.log("in onCreate");
+        $rootScope.waitingForLocation = false;
+    },
+    onStart : function() {},
+    onResume: function() {},
+    onPause: function() {},
+    updateUI: function(travelTime, routeName, updateTime) {},
+    onTimeToGo: function(travelTime, routeName, updateTime) {},
+    onDrivingTime: function() {},
+    onCurrentLocation: function(lat, long) {
+      console.log("in onCurrentLocation");
+      $rootScope.waitingForLocation = false;
+    },
+  }
+
 });
 
-//}]);
