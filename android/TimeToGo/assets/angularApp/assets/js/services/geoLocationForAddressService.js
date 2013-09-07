33a63fbc7db46727f617d2598b8b77af7f2b5a53
yonatanm <yonatanm@gmail.com>
2013-09-07 21:33:18 +0300

    added geoLocationForAddressService

---
+++ android/TimeToGo/assets/angularApp/assets/js/services/geoLocationForAddressService.js
@@ -0,0 +1,14 @@
+//angular.module('timeToGo', []).
+timeToGoApp.
+service('GeoLocationForAddressService', function($http){
+  return {
+    getGeoLocationForAddress: function (address, callback) {
+      var url ="http://maps.googleapis.com/maps/api/geocode/json?sensor=true&language=iw&address="+address;
+      $http.get(url).success( function(data) {
+        // data.results.isEmpty ?
+        var geoLocation = data.results[0].geometry.location;
+        callback(geoLocation);
+      });
+    }
+  }
+});
